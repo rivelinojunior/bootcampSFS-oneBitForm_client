98a7f14b0b0a46dc58aa7b4db94562d15a6d0717
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-25 19:26:59 -0400

    inject service of tokenAuth in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService } from 'angular2-token';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _tokenService: Angular2TokenService) { }
 
   ngOnInit() {
   }
