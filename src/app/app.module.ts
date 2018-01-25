4d44ca25f46953eee705b931c169964ef5b63e94
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-25 17:43:59 -0400

    add angular2-token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
+import { Angular2TokenService } from 'angular2-token';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,7 @@ import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { FormsModule } from './forms/forms.module';
 import { AnswersModule } from './answers/answers.module';
+import { UsersModule } from './users/users.module';
 
 import { RouterModule } from '@angular/router';
 import { routing } from './app.routing';
@@ -24,11 +26,13 @@ import { routing } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    Angular2TokenService,
     MaterializeModule.forRoot(),
     ColorPickerModule,
     ChartsModule,
     FormsModule,
     AnswersModule,
+    UsersModule,
     RouterModule,
     routing
   ],
