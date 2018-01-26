ce1769ac0036cb40fc2b8eac7e53ae42c8d1d6e5
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-25 23:04:17 -0400

    implements logout in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Angular2TokenService } from 'angular2-token';
+import { MzToastService} from 'ng2-materialize';
+
 
 @Component({
   selector: 'app-header',
@@ -8,9 +10,15 @@ import { Angular2TokenService } from 'angular2-token';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private _tokenService: Angular2TokenService) { }
+  constructor(private _tokenService: Angular2TokenService, private toastService: MzToastService) { }
 
   ngOnInit() {
   }
 
+  signOut() {
+    this._tokenService.signOut().subscribe(
+      res =>      { },
+      error =>    this.toastService.show('Error in Logout', 8000, 'red')
+    );
+  }
 }
